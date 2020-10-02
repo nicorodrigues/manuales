fceea84c136a477c19b411834c7d656ce7f6d76a
arbust908@gmail.com <fblanco@digitalhouse.com>
2020-10-01 22:53:37 -0300

    Minor fixes2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,14 +5,12 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Profile</div>
-
                 <div class="panel-body">
                     @if (session('status'))
                         <div class="alert alert-success">
                             {{ session('status') }}
                         </div>
                     @endif
-
                     You are logged in!
                 </div>
             </div>
